87af41f6859cc6bcca1eb30cfd468cc7c2096a08
ssddi456 <torrent88410@gmail.com>
2019-12-05 20:14:03 +0800

    update

--- close_wifi_proxy.py
+++ close_wifi_proxy.py
@@ -16,21 +16,21 @@ parser.add_option("-s", "--series", dest="series",
 (options, args) = parser.parse_args()
 
 deviceSerise = options.series
-
-
+sdeviceSerise = '172.30.21.134:5555'
+print(deviceSerise == sdeviceSerise)
+print(deviceSerise)
 if deviceSerise == None:
     raise 'you muse input series for adb'
 # Popen('adb connect ' + deviceSerise)
 # d.screen.on()
-d = u2.connect(deviceSerise)
 
-Popen('adb -s ' + deviceSerise +
+Popen('adb -s ' + sdeviceSerise +
       ' shell "am start -n com.android.settings/.Settings"')
 
-d = u2.connect(deviceSerise)
+d = u2.connect(sdeviceSerise)
 
 def wait_and_click(**kwargs):
-    d(**kwargs).wait(3)
+    d(**kwargs).wait(1)
     d(**kwargs).click()
 
 print('click wlan setting')
@@ -43,27 +43,16 @@ print('click current wifi detail')
 wait_and_click(className='android.widget.TextView', index=0)
 
 
-d(resourceId='com.android.settings:id/dialog_scrollview').wait(3)
-d(resourceId='com.android.settings:id/dialog_scrollview').scroll.vert.forward(steps=10)
+d(resourceId='com.android.settings:id/dialog_scrollview').wait(1)
 
-print('click current wifi advanted detail')
-wait_and_click(
-    resourceId='com.android.settings:id/wifi_advanced_togglebox')
 d.press("back")
 
-d(resourceId='com.android.settings:id/dialog_scrollview').scroll.vert.forward(steps=10)
+
 print('click current wifi proxy config')
 wait_and_click(
     resourceId='com.android.settings:id/proxy_settings')
 wait_and_click(
-    index=1, className='android.widget.CheckedTextView')
-d(resourceId='com.android.settings:id/dialog_scrollview').scroll.vert.forward(steps=30)
-
-print('setup current wifi proxy config')
-d(resourceId='com.android.settings:id/proxy_hostname').set_text(localip)
-d.press("back")
-d(resourceId='com.android.settings:id/proxy_port').set_text('8888')
-d.press("back")
+    index=0, className='android.widget.CheckedTextView')
 
 wait_and_click(
     index=1, className='android.widget.Button', text="保存")
